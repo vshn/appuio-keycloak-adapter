5dd00536b254d2bd13e4b12c47351eb867395f4b
Fabian Fischer <glrf@users.noreply.github.com>
2022-02-10 13:39:52 +0100

    Refactor Keycloak import

--- controllers/organization_controller_sync.go
+++ controllers/organization_controller_sync.go
@@ -5,6 +5,7 @@ import (
 
 	orgv1 "github.com/appuio/control-api/apis/organization/v1"
 	controlv1 "github.com/appuio/control-api/apis/v1"
+	"github.com/vshn/appuio-keycloak-adapter/keycloak"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -13,6 +14,7 @@ import (
 
 var orgImportAnnot = "keycloak-adapter.vshn.net/importing"
 
+// Sync lists all Keycloak groups in the realm and creates corresponding Organizations if they do not exist
 func (r *OrganizationReconciler) Sync(ctx context.Context) error {
 	log := log.FromContext(ctx)
 
@@ -22,67 +24,87 @@ func (r *OrganizationReconciler) Sync(ctx context.Context) error {
 		return err
 	}
 
-	orgs := orgv1.OrganizationList{}
-	err = r.List(ctx, &orgs)
+	orgMap, err := r.getOrganiztionMap(ctx)
 	if err != nil {
-		log.Error(err, "error listing organizations")
+		log.Error(err, "error listing Organizations")
 		return err
-	}
-
-	orgMap := map[string]*orgv1.Organization{}
-	for i, o := range orgs.Items {
-		orgMap[o.Name] = &orgs.Items[i]
 	}
 
 	for _, g := range gs {
 		org, ok := orgMap[g.Name]
 		if !ok {
 			log.V(1).WithValues("group", g).Info("creating organization")
-			org = &orgv1.Organization{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: g.Name,
-					Annotations: map[string]string{
-						orgImportAnnot: "true",
-					},
-				},
-				Spec: orgv1.OrganizationSpec{
-					DisplayName: g.Name,
-				},
-			}
-			err := r.Create(ctx, org)
+			org, err = r.startImportOrganizationFromGroup(ctx, g)
 			if err != nil {
-				log.WithValues("org", org.Name).Error(err, "failed to create organization")
+				log.WithValues("group", g).Error(err, "failed to start organization import")
 				continue
 			}
 		}
 		if org.Annotations[orgImportAnnot] == "true" {
 			log.V(1).WithValues("group", g).Info("updating organization members")
-			orgMemb := controlv1.OrganizationMembers{}
-			err = r.Get(ctx, types.NamespacedName{
-				Namespace: g.Name,
-				Name:      "members",
-			}, &orgMemb)
+			err := r.updateOrganizationMembersFromGroup(ctx, g)
 			if err != nil {
-				log.WithValues("org", org.Name).Error(err, "failed to get organization members")
+				log.WithValues("group", g).Error(err, "failed to update organization members")
 				continue
 			}
-			orgMemb.Spec.UserRefs = make([]controlv1.UserRef, len(g.Members))
-			for i, m := range g.Members {
-				orgMemb.Spec.UserRefs[i] = controlv1.UserRef{Name: m}
-			}
-			err = r.Update(ctx, &orgMemb)
+			err = r.finishImportOrganizationFromGroup(ctx, org)
 			if err != nil {
-				log.WithValues("org", org.Name).Error(err, "failed to update organization members")
-				continue
-			}
-
-			delete(org.Annotations, orgImportAnnot)
-			err = r.Update(ctx, org)
-			if err != nil {
-				log.WithValues("org", org.Name).Error(err, "failed to update organization")
+				log.WithValues("group", g).Error(err, "failed to finish organization import")
 				continue
 			}
 		}
 	}
 	return nil
+}
+
+func (r *OrganizationReconciler) getOrganiztionMap(ctx context.Context) (map[string]*orgv1.Organization, error) {
+	orgs := orgv1.OrganizationList{}
+	err := r.List(ctx, &orgs)
+	if err != nil {
+		return nil, err
+	}
+
+	orgMap := map[string]*orgv1.Organization{}
+	for i, o := range orgs.Items {
+		orgMap[o.Name] = &orgs.Items[i]
+	}
+	return orgMap, nil
+}
+
+func (r *OrganizationReconciler) startImportOrganizationFromGroup(ctx context.Context, group keycloak.Group) (*orgv1.Organization, error) {
+	org := &orgv1.Organization{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: group.Name,
+			Annotations: map[string]string{
+				orgImportAnnot: "true",
+			},
+		},
+		Spec: orgv1.OrganizationSpec{
+			DisplayName: group.Name,
+		},
+	}
+	err := r.Create(ctx, org)
+	return org, err
+}
+
+func (r *OrganizationReconciler) finishImportOrganizationFromGroup(ctx context.Context, org *orgv1.Organization) error {
+	delete(org.Annotations, orgImportAnnot)
+	return r.Update(ctx, org)
+
+}
+
+func (r *OrganizationReconciler) updateOrganizationMembersFromGroup(ctx context.Context, group keycloak.Group) error {
+	orgMemb := controlv1.OrganizationMembers{}
+	err := r.Get(ctx, types.NamespacedName{
+		Namespace: group.Name,
+		Name:      "members",
+	}, &orgMemb)
+	if err != nil {
+		return err
+	}
+	orgMemb.Spec.UserRefs = make([]controlv1.UserRef, len(group.Members))
+	for i, m := range group.Members {
+		orgMemb.Spec.UserRefs[i] = controlv1.UserRef{Name: m}
+	}
+	return r.Update(ctx, &orgMemb)
 }
